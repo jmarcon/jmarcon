9dadd4720543c129cc6c5f045e002b241fe21704
Juliano Marcon <juliano.marcon@outlook.com>
2016-03-05 16:47:38 -0300

    2016

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+var shell = require('gulp-shell');
+
+var commitCommand = 'git commit -a -m "'+ (new Date().getFullYear()) +'"';
+gulp.task('commit_src', shell.task([
+  'git add .',
+  commitCommand
+]));
 
 gulp.task('css', function() {
   gulp.src('public/less/*.less')
@@ -7,4 +15,13 @@ gulp.task('css', function() {
       .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', ['css']);
+gulp.task('minify-css', function() {
+  return gulp.src('public/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ['css', 'minify-css']);
+
+gulp.task('github', ['default']);
+gulp.task('jm', ['default']);
