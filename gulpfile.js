7521d9ff15f8d8b3721e62d99ee9384f6b19d7c0
Juliano Marcon <juliano.marcon@outlook.com>
2016-03-05 17:16:20 -0300

    Publish Sat Mar 05 2016 17:16:20 GMT-0300 (BRT)

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
 var shell = require('gulp-shell');
+var git = require('gulp-git');
 
 gulp.task('publish_jm', shell.task([
   'sh _publish_jm.sh'
 ]));
 
 gulp.task('publish_git', shell.task([
-  'sh _publish.sh'
+  'hugo -D --config="config.toml"',
+  'cd public',
+  'git add .',
+  'git commit -a -m "Publish $NOW"',
+  'git push -f origin master'
 ]));
 
-gulp.task('commit', shell.task([
-  'sh _commit.sh'
-]));
+gulp.task('commit', function() {
+  gulp.src('.')
+      .pipe(git.add())
+      .pipe(git.commit('Publish ' + (new Date())));
+});
 
 gulp.task('less', function() {
   gulp.src('public/less/*.less')
