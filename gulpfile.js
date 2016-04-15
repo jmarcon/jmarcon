00e97523143ead4786a1c2598d986a135b72824c
Juliano Marcon <juliano.marcon@outlook.com>
2016-04-15 00:52:24 -0300

    Release v1.0.0 : Fri Apr 15 2016 00:52:24 GMT-0300 (BRT)

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,7 @@ gulp.task('default', function(callback) {
   );
 });
 
-gulp.task('github', ['commit-source'], function(callback) {
+gulp.task('github', ['push-source'], function(callback) {
   return sequence(
   'less-github',
   'minify-css-github',
