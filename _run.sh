9308b201ef5d78ec15bd44192eec8a35d8486b3e
Juliano Marcon <juliano.marcon@outlook.com>
2017-02-03 21:55:29 -0200

    automatizar a compilacao

--- _run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-hugo server -Dt hugo-geo
--- run_jm.sh
+++ run_jm.sh
@@ -1,2 +1,3 @@
 #docker run --rm --name "my-hugo" -p 1313:1313 -v $(pwd):/src hugo 
 docker run --rm --name "my-hugo" -p 1313:1313 -v $(pwd):/src -v /Users/jmarcon/Projects/Blog/hugo:/output hugo:jm 
+yes | cp -Rf /Users/jmarcon/Projects/Blog/hugo/ /Users/jmarcon/Projects/Blog/julianomarcon
