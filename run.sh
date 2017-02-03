5b60a6bcf1439b9263c95e76f2ca38b11d68e8b7
Juliano Marcon <juliano.marcon@outlook.com>
2017-02-03 21:10:53 -0200

    Corrigindo compilacao e docker

---
+++ run.sh
@@ -0,0 +1,2 @@
+#docker run --rm --name "my-hugo" -p 1313:1313 -v $(pwd):/src hugo 
+docker run --rm --name "my-hugo" -p 1313:1313 -v $(pwd):/src -v /Users/jmarcon/Projects/Blog/hugo:/output hugo:latest 
