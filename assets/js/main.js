f44b34dd6fe269c8253ad78b58a85d137d033942
Juliano Marcon <1767705+jmarcon@users.noreply.github.com>
2022-10-10 17:37:34 +0000

    Lint

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,15 @@ hljs.initHighlightingOnLoad();
 
 let isDark = false;
 function loadTheme() {
-    
-    var css = document.getElementById("myStyle");
+    let css = document.getElementById("myStyle");
     let theme = localStorage.getItem("theme");
-    if(!theme){
+    if (! theme) {
         theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
         localStorage.setItem("theme", theme);
     }
 
     css.theme = theme;
-    if(theme === "dark") {
+    if (theme === "dark") {
         css.href = "/assets/css/maindark.css";
     } else {
         css.href = "/assets/css/main.css";
@@ -23,10 +22,9 @@ loadTheme();
 function cssToggle(e) {
     e.preventDefault();
     let theme = localStorage.getItem("theme");
-    if(theme === "dark") {
+    if (theme === "dark") {
         localStorage.setItem("theme", "light");
-    }else{
+    } else {
         localStorage.setItem("theme", "dark");
-    }
-    loadTheme()
+    } loadTheme()
 }
